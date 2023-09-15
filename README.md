4eaa374666d6dd7a031535e54b6d738843299b9a
atarico9 <atarico9@gmail.com>
2023-09-15 19:47:38 -0300

    feat: Installation of ESLint with coding standard, configuration of .prettierrc for code formatting, and addition of README.md with the technical test guidelines
    - Installed ESLint with the coding standard.
    - Created and modified the .prettierrc file to establish code formatting rules.
    - Added a README.md file with the guidelines and description of the technical test.

---
+++ README.md
@@ -0,0 +1,4 @@
+- Facts random: https://catfact.ninja/fact
+- Imagen random: https://cataas.com/cat/says/hello
+
+Recuperar un hecho aleatorio de gatos de la primera API y mostrar una img de un gato, con la primera palabra del hecho recuperado usando la segunda API
--- main.jsx
+++ main.jsx
@@ -1,5 +1,5 @@
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client'
 
-const root = createRoot(document.getElementById("app"));
+const root = createRoot(document.getElementById('app'))
 
-root.render(<h1>HOLA MUNDOOOOO</h1>);
+root.render(<h1>HOLA MUNDOOOOO</h1>)
